becef5d03ba74034cd875521362fe553e5c1fac6
w3b3ye <w3b3ye@gmail.com>
2021-07-10 10:02:39 +0000

    Voting app - final working version.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,13 +8,17 @@ contract Election {
         uint256 voteCount;
     }
 
-    // Read/write candidate
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
     mapping(uint256 => Candidate) public candidates;
-
     // Store Candidates Count
     uint256 public candidatesCount;
 
-    // Constructor
+    // voted event
+    event votedEvent(uint256 indexed _candidateId);
+
     constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
@@ -23,5 +27,22 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
